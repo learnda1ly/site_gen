136c9c93af81a57bd614c93363228e5fb019d60f
learnda1ly <stephen.a.quinlan@gmail.com>
2026-01-01 17:19:05 -0600

    Added additional test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,17 +4,31 @@ from textnode import TextNode, TextType
 
 
 class TestTextNode(unittest.TestCase):
-    def test_eq(self):
+    def test_valid_eq(self):
 
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
 
         self.assertEqual(node, node2)
 
-    def test_neq(self):
+    def test_text_makes_neq(self):
 
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is A text node", TextType.BOLD)
+
+        self.assertNotEqual(node, node2)
+
+    def test_texttype_makes_neq(self):
+
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.LINK)
+
+        self.assertNotEqual(node, node2)
+
+    def test_url_makes_neq(self):
+
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is A text node", TextType.BOLD, "www.boot.dev")
 
         self.assertNotEqual(node, node2)
 
